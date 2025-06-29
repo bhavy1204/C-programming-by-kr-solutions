d731024637bacb6c9a137d1ecef11ec127126904
bhavy1204 <bhavysarwa51@gmail.com>
2025-06-29 21:01:45 +0530

    solve examples and question of section 1.5

--- chapter 1/examples-1.c
+++ chapter 1/examples-1.c
@@ -24,12 +24,81 @@
 
 //--------------------------------------Section 1.3 The for statement--------------------------------------
 
+// #include <stdio.h>
+// int main()
+// {
+//     for (int fahr = 0; fahr < 300; fahr=fahr+20){
+//         printf("%3d %8.2f\n", fahr, (5.0/9.0)* (fahr-32));
+//     }
+
+//     return 0;
+// }
+
+//--------------------------------------------------------------------------------------------
+
+// #include <stdio.h>
+// int main()
+// {
+//     int c; //Try to makke it to char and observe
+
+//     c = getchar();
+//     while (c != EOF)
+//     {
+//         putchar(c);
+//         c = getchar();
+//     }
+//     return 0;
+// }
+
+//--------------------------------------------------------------------------------------------
+
+// #include <stdio.h>
+// int main()
+// {
+//     int c;
+//     while ((c = getchar()) != EOF)
+//         putchar(c);
+
+//     return 0;
+// }
+
+//--------------------------------------------------------------------------------------------
+
+// #include <stdio.h>
+// int main()
+// {
+//     long nc;
+//     nc = 0;
+//     while (getchar() != EOF)
+//         ++nc;
+//     printf("%d", nc);
+//     return 0;
+// }
+
+//--------------------------------------------------------------------------------------------
+
+// #include <stdio.h>
+// int main()
+// {
+//     double cnt;
+
+//     for (cnt = 0; getchar() != EOF; ++cnt);
+//     printf("%f", cnt);
+
+//     return 0;
+// }
+
+//--------------------------------------------------------------------------------------------
+
 #include <stdio.h>
 int main()
 {
-    for (int fahr = 0; fahr < 300; fahr=fahr+20){
-        printf("%3d %8.2f\n", fahr, (5.0/9.0)* (fahr-32));
-    }
+    int lineCnt, c;
+    lineCnt = 0;
+    while ((c = getchar()) != EOF)
+        if (c == '\n')
+            ++lineCnt;
+    printf("%d", lineCnt);
 
     return 0;
 }
--- chapter 1/exercise-1.c
+++ chapter 1/exercise-1.c
@@ -14,21 +14,34 @@
 //     printf("hello world");
 // }
 
-
 //-------------------------------------Exercise 1.5-------------------------------------------
 
-#include <stdio.h>
+// #include <stdio.h>
 // #define LOWER 0; //try this as well and observe the error
-#define LOWER 0
-#define UPPER 300
-#define STEP 20
-#define H "Hello"
+// #define LOWER 0
+// #define UPPER 300
+// #define STEP 20
+// #define H "Hello"
+// int main()
+// {
+//     printf("%s", H);
+//     for (int fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP){
+//         printf("%3d %8.2f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+//     }
+
+//     return 0;
+// }
+
+//----------------------------------------1.6 + 1.7 -----------------------------------------
+
+#include <stdio.h>
 int main()
 {
-    printf("%s", H);
-    for (int fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP){
-        printf("%3d %8.2f\n", fahr, (5.0 / 9.0) * (fahr - 32));
-    }
+    int c;
 
+    // Press ctrl + z or ctrl + D and observe
+    c = getchar();
+    printf("%d", (c != EOF));
+    printf("\nValue of EOF : %d", EOF);
     return 0;
 }
